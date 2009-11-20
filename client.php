47fea20071d3fbc4f3c604ec71fac93cb94a22c9
fvs <fvs@30e9dc4a-7611-4fbc-83ba-eecf79b76bd0>
2009-11-20 14:20:18 +0000

    Copyright
    git-svn-id: svn://oss.beweb.dbc.dk/sync_php/php/OpenLibrary/OpenSearch/trunk@7058 30e9dc4a-7611-4fbc-83ba-eecf79b76bd0

--- client.php
+++ client.php
@@ -1,23 +1,24 @@
 <?php
 /**
  *
- * This file is part of OpenSearch.
+ * This file is part of Open Library System.
  * Copyright © 2009, Dansk Bibliotekscenter a/s,
  * Tempovej 7-11, DK-2750 Ballerup, Denmark. CVR: 15149043
  *
- * OpenSearch is free software: you can redistribute it and/or modify
+ * Open Library System is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * OpenSearch is distributed in the hope that it will be useful,
+ * Open Library System is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with OpenSearch.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Open Library System.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 
 define("WSDL", "opensearch.wsdl");
 
