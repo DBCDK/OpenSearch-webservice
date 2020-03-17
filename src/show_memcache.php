a550db9f065ae871bd0477da5f6270d26dd1624e
Mads Bondo Dydensborg <mabd@dbc.dk>
2020-03-17 17:15:33 +0100

    SE-2929: Full support for Redis info output in show cache page

--- src/show_memcache.php
+++ src/show_memcache.php
@@ -17,20 +17,23 @@ tr3_me("<em>Object type</em>", "<em>Cache type</em>", "<em>Cache host:port</em>"
 tr3_me("aaa/fors & solr_file", "memcache internal to pod", "localhost:11211");
 tr3_me("OpenAgency/VIP", "Redis cache", trim(getenv("AGENCY_CACHE_HOST"), '"') . ":" . trim(getenv("AGENCY_CACHE_PORT"), '"'));
 tr3_me("SOLR and COREPO objects", "Redis cache", trim(getenv("CACHE_HOST"), '"') . ":" . trim(getenv("CACHE_PORT"), '"'));
+echo "</table></p>";
 
-echo "<p>OpenSearch *always* use a local memcache at 11211 for aaa/fors and solr_file entries. Due to the very static nature of this cache, the hit ratios on this cache will often be >90%.</p>";
-echo "<p>OpenSearch *always* use a redis cache for OpenAgency/VIP/SOLR/COREPO objects.</p>";
+echo "<p>OpenSearch <em>always</em> use a local memcache at 11211 for aaa/fors and solr_file entries. Due to the very static nature of this cache, the hit ratios on this cache will often be >90%.</p>";
+echo "<p>OpenSearch <em>always</em> use a redis cache for OpenAgency/VIP and SOLR/COREPO objects. These caches can be different, but this script is too stupid to know this.</p>";
+
+// There are more information about the stats to get out of redis here: https://github.com/phpredis/phpredis#info
+// and here: https://redis.io/commands/info
 
 echo "<h2>OpenAgency/VIP cache info (AGENCY_CACHE_HOST)</h2>";
 $rediscache_obj = new Redis();
 $rediscache_obj->connect(trim(getenv("AGENCY_CACHE_HOST"), '"'), trim(getenv("AGENCY_CACHE_PORT"), '"') );
-rediscache_info($rediscache_obj->info());
-/*
+rediscache_info($rediscache_obj->info("all"));
+
 echo "<h2>SOLR/COREPO cache info (CACHE_HOST)</h2>";
-$memcache_obj = new Memcache;
-$memcache_obj->connect(trim(getenv("CACHE_HOST"), '"'), trim(getenv("CACHE_PORT"), '"') );
-cache_info($memcache_obj->getStats());
-*/
+$rediscache_obj = new Redis();
+$rediscache_obj->connect(trim(getenv("CACHE_HOST"), '"'), trim(getenv("CACHE_PORT"), '"') );
+rediscache_info($rediscache_obj->info("all"));
 
 echo "<h2>aaa/fors & solr_file cache info (localhost:11211)</h2>";
 $memcache_obj = new Memcache;
@@ -86,7 +89,74 @@ tr2_me("Number of bytes currently in storage.",$MBSize." Mega Bytes");
 
 function rediscache_info($status){
   echo "<table border='1'>";
-  tr2_me("Rediscache Server version: ", $status ["redis_version"]);
+
+  $interessting = array(
+    "redis_version" => "Redis version",
+    "redis_mode" => "Redis mode",
+    "uptime_in_seconds" => "Uptime in seconds",
+    "uptime_in_days" => "Uptime in days",
+    "connected_clients" => "Connected clients",
+    "used_memory_human" => "Used memory",
+    "used_memory_rss_human" => "Used memory resident",
+    "used_memory_peak_human" => "Used memory peak",
+    "used_memory_peak_perc" => "Used memory peak percentage",
+    "used_memory_overhead" => "Used memory overhead",
+    "used_memory_startup" => "Used memory startup",
+    "used_memory_dataset" => "Used memory dataset",
+    "used_memory_dataset_perc" => "Used memory data percentage",
+    "total_system_memory_human" => "Total system memory",
+    "used_memory_lua_human" => "Used memory lua",
+    "used_memory_scripts_human" => "Used memory scripts",
+    "maxmemory_human" => "Max memory",
+    "maxmemory_policy" => "Max memory policy",
+    "expired_keys"=> "Expired keys",
+    "expired_stale_perc" => "Expired stale percentage",
+    "expired_time_cap_reached_count" => "Expired time cap reached count",
+    "evicted_keys" => "Evicted keys",
+    "used_cpu_sys" => "Used CPU system",
+    "used_cpu_user" => "Used CPU user",
+    "used_cpu_sys_children" => "Used CPU system (children)",
+    "used_cpu_user_children" => "Used CPU user (children)",
+    "cmdstat_get" => "Get command stats",
+    "cmdstat_info	calls" => "Info command stats",
+    "cmdstat_setex" => "Set(ex) command stats",
+    "cmdstat_command" => "Command command stats",
+    "cluster_enabled"	=> "Cluster enabled",
+    "db0" => "DB0 stats",
+    "db1" => "DB0 stats",
+    "db2" => "DB0 stats",
+    "db3" => "DB0 stats",
+    "db4" => "DB0 stats",
+    "db5" => "DB0 stats",
+    "db6" => "DB0 stats",
+    "db7" => "DB0 stats",
+    "db8" => "DB0 stats",
+    "db9" => "DB0 stats"
+
+    // "keyspace_hits" => "Keyspace hits",
+    // "keyspace_misses" => "Keyspace misses"
+  );
+
+  // Translate the keys we have, ignore the rest.
+  foreach ($status as $key => $value) {
+    if (array_key_exists($key, $interessting)) {
+      tr2_me($interessting[$key], $value);
+    } else {
+      // Enable if you wish to see all values.
+      //tr2_me($key, $value);
+    }
+  }
+
+  // Some calculated stats, made to match the stuff from the memcache caches.
+  $total_gets = (real) $status["keyspace_hits"] + (real) $status["keyspace_misses"];
+  $percCacheHit = ((real)$status["keyspace_hits"]) / $total_gets * 100.0;
+  $percCacheHit=round($percCacheHit,2);
+  $percCacheMiss=100-$percCacheHit;
+
+  tr2_me("Cumulative number of retrieval requests", $total_gets);
+  tr2_me("Number of keys that have been requested and found present ", $status ["keyspace_hits"]." ($percCacheHit%)");
+  tr2_me("Number of items that have been requested and not found ", $status ["keyspace_misses"]." ($percCacheMiss%)");
+
 
   echo "</table>";
 }
