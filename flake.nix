23871be903e01fc78823f33dbd78b463cfd46b9a
Jacob Christian Poulsen <jp@dbc.dk>
2023-04-17 11:02:07 +0200

    Cleanup flake: remove uncommented code Use pkgs.lib.hasSuffix to test for darwin systems add name to develop shell

--- flake.nix
+++ flake.nix
@@ -1,29 +1,22 @@
 {
-  description = "opensearch php dev shell";
+  description = "opens earch php dev shell";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
   };
-  outputs = inputs@{ flake-parts, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
-      flake = {
-        # Put your original flake attributes here.
-      };
-      systems = [
-        # systems for which you want to build the `perSystem` attributes
-        "x86_64-linux"
-        "aarch64-darwin"
-        # ...
-      ];
-      perSystem = { config, pkgs, system, ... }: 
+      systems = nixpkgs.lib.systems.flakeExposed ;
+      perSystem = { config, pkgs, system, ... }:
       let
         osPHP = pkgs.php.withExtensions ({ enabled, all }: enabled ++ [ all.imagick all.memcached ]);
-        darwin-packages = pkgs.lib.lists.optional ( system=="aarch64-darwin" ) pkgs.darwin.iproute2mac; 
+        isDarwin = pkgs.lib.hasSuffix "darwin" system;
+        darwin-packages = pkgs.lib.lists.optional isDarwin pkgs.darwin.iproute2mac;
       in {
         devShells.default = pkgs.mkShell {
-                buildInputs = [ osPHP osPHP.packages.composer pkgs.subversion ] ++ darwin-packages; 
+                name = "open search";
+                buildInputs = [ osPHP osPHP.packages.composer pkgs.subversion ] ++ darwin-packages;
         };
       };
     };
