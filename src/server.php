1083e81410c2fbb2a34157ed7673ca67803f4a65
Morten Bøgeskov <mb@dbc.dk>
2023-06-14 09:59:37 +0200

    Removed collection_alias from code

--- src/server.php
+++ src/server.php
@@ -60,7 +60,6 @@ class OpenSearch extends webServiceServer {
   protected $search_filter_for_800000 = [];  // set when collection 800000-danbib or 800000-bibdk are searchable
   protected $collection_contained_in = [];
   protected $rank_frequence_debug;
-  protected $collection_alias = [];
   protected $feature_sw = [];
   protected $user_param;
   protected $debug_query = FALSE;
@@ -1323,7 +1322,6 @@ class OpenSearch extends webServiceServer {
   private function set_solr_filter($profile, $add_relation_sources = FALSE) {
     $ret = [];
     if (is_array($profile)) {
-      $this->collection_alias = self::set_collection_alias($profile);
       foreach ($profile as $p) {
         if (self::xs_boolean($p['sourceSearchable']) || ($add_relation_sources && isset($p['relation']) && count($p['relation']))) {
           $source_id = $p['sourceIdentifier'] ?? '';
@@ -1386,7 +1384,6 @@ class OpenSearch extends webServiceServer {
   private function split_collections_for_holdingsitem($profile, $add_relation_sources = FALSE) {
     $filtered_collections = $normal_collections = [];
     if (is_array($profile)) {
-      $this->collection_alias = self::set_collection_alias($profile);
       foreach ($profile as $p) {
         $source_id = $p['sourceIdentifier'] ?? '';
         if (self::xs_boolean($p['sourceSearchable']) || ($add_relation_sources && count($p['relation']))) {
@@ -1401,30 +1398,6 @@ class OpenSearch extends webServiceServer {
     return
       ($normal_collections ? FIELD_COLLECTION_INDEX . ':(' . implode(' OR ', $normal_collections) . ') OR ' : '') .
       ($filtered_collections ? '(' . FIELD_COLLECTION_INDEX . ':(' . implode(' OR ', $filtered_collections) . ') AND %s)' : '%s');
-  }
-
-  /** \brief Set list of collection alias' depending on the user search profile
-   * - in repository: ['collection_alias']['870876-anmeld'] = '870976-allanmeld';
-   *
-   * @param array $profile - the users search profile
-   * @return array - collection alias'
-   */
-  private function set_collection_alias($profile) {
-    $collection_alias = [];
-    $r_cl = $this->repository['collection_alias'] ?? '';
-    $alias = is_array($r_cl) ? array_flip($r_cl) : [];
-    foreach ($profile as $p) {
-      if (self::xs_boolean($p['sourceSearchable'])) {
-        $si = $p['sourceIdentifier'];
-        if (empty($alias[$si])) {
-          $collection_alias[$si] = $si;
-        }
-        elseif (empty($collection_alias[$alias[$si]])) {
-          $collection_alias[$alias[$si]] = $si;
-        }
-      }
-    }
-    return $collection_alias;
   }
 
 
@@ -3302,9 +3275,6 @@ class OpenSearch extends webServiceServer {
    */
   private function extract_record(&$dom, $rec_id) {
     $record_source = self::record_source_from_pid($rec_id);
-    if (isset($this->collection_alias[$record_source])) {
-      $record_source = $this->collection_alias[$record_source];
-    }
     $ret = new stdClass();
     foreach ($this->format as $format_name => $format_arr) {
       switch ($format_name) {
