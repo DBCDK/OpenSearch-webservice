c16c13eccb4b87a073b8c78ed89d702d1701f360
Jacob Christian Poulsen <jp@dbc.dk>
2023-09-07 14:02:23 +0200

    Depend on build docker nix flake

--- flake.nix
+++ flake.nix
@@ -4,8 +4,9 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-22.11";
     flake-parts.url = "github:hercules-ci/flake-parts";
+    build-tools.url = "git+ssh://gitlab@gitlab.dbc.dk/i-scrum/build-tools.git";
   };
-  outputs = inputs@{ flake-parts, nixpkgs, ... }:
+  outputs = inputs@{ flake-parts, nixpkgs, build-tools, ... }:
     flake-parts.lib.mkFlake { inherit inputs; } {
       systems = nixpkgs.lib.systems.flakeExposed ;
       perSystem = { config, pkgs, system, ... }:
@@ -13,10 +14,11 @@
         osPHP = pkgs.php.withExtensions ({ enabled, all }: enabled ++ [ all.imagick all.memcached ]);
         isDarwin = pkgs.lib.hasSuffix "darwin" system;
         darwin-packages = pkgs.lib.lists.optional isDarwin pkgs.darwin.iproute2mac;
+        build-docker = build-tools.packages.${system}.default;
       in {
         devShells.default = pkgs.mkShell {
                 name = "open search";
-                buildInputs = [ osPHP osPHP.packages.composer pkgs.subversion ] ++ darwin-packages;
+                buildInputs = [ osPHP osPHP.packages.composer pkgs.subversion build-docker ] ++ darwin-packages;
         };
       };
     };
